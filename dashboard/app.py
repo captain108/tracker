326c8dbe77bf358fb47a2dc4ec3457328bf9c835
Captain <papajiop11@gmail.com>
2025-08-01 14:23:58 +0530

    Create app.py

---
+++ dashboard/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from utils.tracking_utils import load_data
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    data = load_data()
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
