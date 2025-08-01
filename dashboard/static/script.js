0ee920437661b7686ca60c7da54804a59291998f
Captain <papajiop11@gmail.com>
2025-08-01 14:32:36 +0530

    Update script.js

--- dashboard/static/script.js
+++ dashboard/static/script.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll("table tr").forEach(row => {
+  let cell = row.cells[1];
+  if (!cell) return;
+  if (cell.innerText.includes("online")) cell.style.color = "green";
+  if (cell.innerText.includes("offline")) cell.style.color = "red";
+});
